23aed173523ae3c40cfd3291945c96af982593fd
Spikey <138967412+Spikree@users.noreply.github.com>
2023-10-24 21:34:50 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <calculatorVue/>
+</template>
+
+<script>
+import calculatorVue from './components/calculatorVue.vue'
+
+export default {
+  name: 'App',
+  components: {
+    calculatorVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
